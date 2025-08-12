85fa9659a302181348538d0bbf8bb39513b84ee2
pective <pective@proton.me>
2025-08-12 18:26:48 +0200

    Tryna make it look better

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,12 +2,13 @@ const container = document.querySelector(".project-list")
 
 export function createProjectList(projects) {
     for (let i = 0; i < projects.length; i++) {
-        const projectElement = document.createElement("div");
+        const projectElement = document.createElement("li");
+        projectElement.textContent = projects[i].name;
 
-        const projectTitle = document.createElement("div");
-        projectTitle.textContent = projects[i].name;
+        const projectRemove = document.createElement("button");
+        projectRemove.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>`;
 
-        projectElement.appendChild(projectTitle);
+        projectElement.appendChild(projectRemove);
         container.appendChild(projectElement);
     }
 }
