2c94bf01cc7090c6e13a04467b37953872f82493
pective <pective@proton.me>
2025-08-21 13:05:21 +0200

    oh my god the dialog works

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import "./style.css";
 import Task from "./modules/task";
 import Project from "./modules/projects";
 import App from "./modules/app";
-import { createProjectList } from "./modules/dom";
+import { createProjectList, createTask } from "./modules/dom";
+
+const dialog = document.querySelector("#taskDialog");
+
+document.querySelector("#addTaskButton").addEventListener("click", (e) => {
+    dialog.showModal();
+    e.preventDefault();
+})
 
 const appControl = new App();
 appControl.createProject(new Project("Home"));
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,4 +1,6 @@
-const container = document.querySelector(".project-list")
+import { format } from "date-fns";
+
+const projectContainer = document.querySelector(".project-list")
 
 export function createProjectList(projects) {
     for (let i = 0; i < projects.length; i++) {
@@ -9,7 +11,40 @@ export function createProjectList(projects) {
         projectRemove.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>`;
 
         projectElement.appendChild(projectRemove);
-        container.appendChild(projectElement);
+        projectContainer.appendChild(projectElement);
     }
 }
 
+
+export function createTask() {
+    const tasksContainer = document.querySelector(".task-list");
+    
+    const task = {
+        title: document.querySelector("#taskDialog input[name=[taskTitle]").value,
+        description: document.querySelector("#taskDialog input[name=[taskDescription]").value,
+        dueDate: document.querySelector("#taskDialog input[name=[taskDueDate]").value,
+        priority: document.querySelector("#taskDialog input[name=[taskPriority]").value
+    }
+
+    const taskBody = document.createElement("div");
+    taskBody.classList.add("")
+
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = task.title;
+
+    const descElement = document.createElement("p");
+    descElement.textContent = task.description;
+
+    const dateElement = document.createElement("h3")
+    dateElement.textContent = format(task.dueDate, 'P');
+
+    const priorityElement = document.createElement("div");
+    priorityElement.textContent = task.priority;
+
+    taskBody.appendChild(titleElement);
+    taskBody.appendChild(descElement);
+    taskBody.appendChild(dateElement);
+    taskBody.appendChild(priorityElement);
+
+    tasksContainer.appendChild(taskBody);
+}
