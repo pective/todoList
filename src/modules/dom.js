b0f35d8215e9c8d1f7860fa4ffa2da0e9a1a0991
pective <pective@proton.me>
2025-08-31 19:14:52 +0200

    Clear the tasklist before rendering it

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -45,9 +45,10 @@ export class DOMController {
     static #buildProjectElement(project) {
         const projectElement = document.createElement("li");
         projectElement.textContent = project.name;
+        projectElement.classList.add("project-element");
 
         const projectRemove = document.createElement("button");
-        projectRemove.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>`;
+        projectRemove.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24"><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>`;
 
         projectElement.appendChild(projectRemove);
         return projectElement;
@@ -75,6 +76,7 @@ export class DOMController {
 
     static createTaskList(taskList) {
         const tasksContainer = document.querySelector(".task-list");
+        tasksContainer.innerHTML = "";
         const fragment = document.createDocumentFragment();
         for (let task of taskList) {
             fragment.appendChild(this.#buildTaskElement(task));
